02b9413bcd28e45ed9b4ba89fc581f513fcde0d6
k-nizy <k.nizeyiman@gmail.com>
2025-05-09 06:52:46 -0700

    1

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-# This script matches strings starting with 'hb', followed by 2-3 't's, and ending with 'n'
+# Matches strings starting with 'hb', followed by any number of 't's, ending with 'n'
 
 input = ARGV[0] || ""
-matches = input.match(hbtt+n)
+matches = input.match(/^hbt*n$/)
 
 puts matches ? matches[0] : ""
