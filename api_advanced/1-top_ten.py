828e0389af528e8b8095d68e2c41b6fe9cbe446c
k-nizy <k.nizeyiman@gmail.com>
2025-07-03 15:50:32 -0700

    addded the mains

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,25 +9,20 @@ import requests
 def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts for a given subreddit.
-    Prints None if the subreddit is invalid or request fails.
+    Does nothing if the subreddit is invalid or request fails.
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
-        "User-Agent": "Moz; MyRedditBot/1.0; +https://github.com/yourusername)"
+        "User-Agent": "Mozilla/5.0 (cot/1.0; +https://github.com/yourusername)"
     }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
-            print(None)
             return
 
-        data = response.json().get("data", {}).get("children", [])
-        if not data:
-            print(None)
-            return
-
-        for post in data:
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts:
             print(post.get("data", {}).get("title"))
     except requests.RequestException:
-        print(None)
+        return
